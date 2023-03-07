2492302709bd96a85901ab7367d778394d19840b
jlorebu <jeibeltranu@gmail.com>
2023-03-07 15:24:37 -0500

    ultima actualizacion parcial

--- BeltranJeimyParcial/src/Components/Card.jsx
+++ BeltranJeimyParcial/src/Components/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({nombre, apto}) => {
 
   return (
     <div>
-         <h3>Señor (a) {nombre} </h3>
+         <h3>Señor(a) {nombre} </h3>
          <h4>Inscribió exitosamente el apartamento: {apto} </h4>
     </div>
   )
